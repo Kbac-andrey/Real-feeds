e893015a78481e7c29a0a3d51fc09e68ce611c3c
andrii.p.marchenko <marchenko1993.a@gmail.com>
2018-04-16 16:00:25 +0300

    made all response

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -8,8 +8,13 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
 
   constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  loginUser() {
+    event.preventDefault()
+    const target = event.target
+    const userEmail = target.querySelector('#userEmail').value
+    const userPassword = target.querySelector('#userPassword').value
+    console.log(userEmail, userPassword);
   }
-
 }
