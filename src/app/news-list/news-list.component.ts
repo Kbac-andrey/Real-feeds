1704d2e7cd351ae1cc3004cccfc61c04b1e66879
andrii.p.marchenko <marchenko1993.a@gmail.com>
2018-04-26 20:52:55 +0300

    added likes icons and fixed bugs

--- src/app/news-list/news-list.component.ts
+++ src/app/news-list/news-list.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Input, Output } from '@angular/core';
 import { HttpClient} from '@angular/common/http';
 import { NewsService } from '../services/news-service.service';
+import {AuthService} from '../services/auth.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-news-list',
@@ -9,7 +11,21 @@ import { NewsService } from '../services/news-service.service';
 })
 export class NewsListComponent implements OnInit {
   public news: object;
-  constructor( private http: HttpClient, private newsService: NewsService) { }
+  logInuser: string[] = [];
+  public StatusMessage: boolean = true;
+  // @Input() article: Article;
+  @Output() onToggle = new EventEmitter<boolean>();
+  constructor( private http: HttpClient, private newsService: NewsService, public authService: AuthService, private router: Router) { }
+  toggleFavorite() {
+    this.logInuser = this.authService.findgetLoggedUser();
+      console.log(this.logInuser);
+      if (this.logInuser.length === 0) {
+        this.router.navigate(['/login']);
+      }
+  }
+
+
+
 
   ngOnInit() {
     this.newsService.fetchNews().subscribe(
