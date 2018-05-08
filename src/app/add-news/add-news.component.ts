828959a363a34eacef5bc4d65fd637b7578f0779
andrii.p.marchenko <marchenko1993.a@gmail.com>
2018-05-08 18:44:01 +0300

    change render in "add news" taken user

--- src/app/add-news/add-news.component.ts
+++ src/app/add-news/add-news.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {AuthService} from '../services/auth.service';
+import {Subscription} from 'rxjs/Subscription';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-add-news',
@@ -6,10 +10,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-news.component.css']
 })
 export class AddNewsComponent implements OnInit {
-
-  constructor() { }
+  private routeSubscriptiontwo: Subscription;
+  public userId: number;
+  public user: object = {};
+  public article: object = {}
+  constructor( private http: HttpClient, public authService: AuthService, private activateRoute: ActivatedRoute, private router: Router) {
+    this.routeSubscriptiontwo = activateRoute.params.subscribe(params => this.userId = params['id']);
+  }
+  addNews(newsdata): void {
+    this.article = {
+      'title': newsdata.inputTitleNews,
+      'lorem': newsdata.inputTextNews,
+      'userLikes': []
+    };
+    this.authService.getUserById(this.userId).subscribe(user => {
+      console.log(this.userId)
+      console.log(user)
+      this.user = user;
+      console.log(this.user)
+    });
+  }
 
   ngOnInit() {
+
   }
 
 }
