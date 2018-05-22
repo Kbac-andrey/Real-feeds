9f760ea9d32885c064bd8494d5bb913bfeb779e6
andrii.p.marchenko <marchenko1993.a@gmail.com>
2018-05-22 18:27:59 +0300

    user can edit article, fixed response for footer

--- src/app/user-area/user-area.component.ts
+++ src/app/user-area/user-area.component.ts
@@ -1,23 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {trigger, state, animate, transition, style} from "@angular/animations";
+// import {trigger, state, animate, transition, style} from "@angular/animations";
 import {ActivatedRoute, Params, Router} from '@angular/router';
 import {AuthService} from '../services/auth.service';
 import {Subscription} from 'rxjs/Subscription';
 import {NewsService} from '../services/news-service.service';
+import {ConfirmationWindowComponent} from '../confirmation-window/confirmation-window.component';
+
 
 @Component({
   selector: 'app-user-area',
   templateUrl: './user-area.component.html',
   styleUrls: ['./user-area.component.css'],
-  animations: [
-    trigger('visibilityChanged', [
-      state('true', style({ display: 'block', opacity: 1, transform: 'scale(1.0)' })),
-      state('false', style({ display: 'none', opacity: 0, transform: 'scale(0.0)'  })),
-      transition('1 => 0', animate('900ms')),
-      transition('0 => 1', animate('900ms'))
-    ])
-  ],
+  // animations: [
+  //   trigger('visibilityChanged', [
+  //     state('show', style({ display: 'block', opacity: 1, transform: 'scale(1.0)' })),
+  //     state('hide', style({ display: 'none', opacity: 0, transform: 'scale(0.0)'  })),
+  //     transition('show => hide', animate('900ms')),
+  //     transition('hide => show', animate('900ms'))
+  //   ])
+  // ],
 })
 export class UserAreaComponent implements OnInit {
   public showEditFormArray: any[] = []
@@ -30,15 +32,19 @@ export class UserAreaComponent implements OnInit {
   private routeSubscription: Subscription;
 
   constructor(private authService: AuthService, private newsService: NewsService, private router: Router, private activateRoute: ActivatedRoute) {
+
     this.routeSubscription = activateRoute.params.subscribe(params => this.userId = params['id']);
+
   }
-  setValueForToggleArticle() {
-    for (let i = 0; i < this.user['articles'].length; i++) {
-      this.showEditFormArray.push(false)
-    }
-  }
+
+  // setValueForToggleArticle() {
+  //   for (let i = 0; i < this.user['articles'].length; i++) {
+  //     this.showEditFormArray.push('hide');
+  //   }
+  // }
   toggleforEditArticle (i) {
-    this.showEditFormArray[i] = !this.showEditFormArray[i];
+    // this.showEditFormArray[i] = this.showEditFormArray[i] === 'show' ? 'hide' : 'show';
+    this.showEditFormArray[i] = !this.showEditFormArray[i]
   }
 
   checkUserstatus() {
@@ -58,7 +64,19 @@ export class UserAreaComponent implements OnInit {
     this.newsService.deleteNews(this.userId, this.user).subscribe( user => {
     });
   }
-
+  editArticle(changedArticle) {
+    let newArticle = {
+      'title': changedArticle.lorem,
+      'lorem': changedArticle.title,
+    };
+    this.authService.editRegestrationUser(this.userId, this.user).subscribe(user => {
+      this.logInuser.push(user)
+      localStorage.setItem('LogInUser', JSON.stringify(this.logInuser));
+    });
+    for (let i = 0; i < this.showEditFormArray.length; i++){
+      this.showEditFormArray[i] = false;
+    }
+  }
   editUser(userData) {
     this.userDataForm = {
       'avatar': this.user['avatar'],
@@ -80,12 +98,13 @@ export class UserAreaComponent implements OnInit {
   ngOnInit() {
     this.authService.getUserById(this.userId).subscribe(user => {
       this.user = user;
-      this.setValueForToggleArticle()
+        // this.setValueForToggleArticle();
       this.checkUserstatus();
     });
     this.checkUserstatus();
     this.newsService.fetchAllInformattion().subscribe(allUsersWithArticles => {
       this.usersWthLikes = allUsersWithArticles;
     });
+
   }
 }
